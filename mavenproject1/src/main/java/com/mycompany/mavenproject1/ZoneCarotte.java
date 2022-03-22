54256bf3f2e55bb470f282d65746634e46739c08
arno00sdq <ad.dudes@outlook.fr>
2022-03-22 18:49:37 +0100

    detection auto add

--- mavenproject1/src/main/java/com/mycompany/mavenproject1/ZoneCarotte.java
+++ mavenproject1/src/main/java/com/mycompany/mavenproject1/ZoneCarotte.java
@@ -5,12 +5,67 @@
  */
 package com.mycompany.mavenproject1;
 
+import java.util.List;
+
 /**
  *
  * @author MSI
  */
 public class ZoneCarotte {
     
+    int upper_x;
+    int upper_y;
+    int lower_x;
+    int lower_y;
+    int w;
+    int h;
+    
+    public ZoneCarotte(int upper_x,int upper_y,int lower_x,int lower_y,int w,int h){
+        
+        this.upper_x = upper_x;
+        this.upper_y = upper_y;
+        this.lower_x = lower_x;
+        this.lower_y = lower_y;
+        this.w = w;
+        this.h = h;
+   
+    }
+    
+    public int getArea(){
+        
+        return this.w * this.h;
+        
+    }
+    
+    public boolean equals(ZoneCarotte other){
+
+        if(this.upper_x == other.upper_x){
+            
+            return true;
+            
+        }else{
+        
+            return false;
+            
+        }
+    }
+    
+    public boolean existsInArray(List<ZoneCarotte> array){
+        
+        boolean exists = false;
+
+        for(int i = 0; i < array.size(); i++ ){
+            
+            if(this.equals(array.get(i))){
+                
+                exists = true;
+            }
+            
+        }
+        
+        return exists;
+        
+    }
     
     
 }
--- mavenproject1/src/main/java/com/mycompany/mavenproject1/automatic_detection_w_target_v2.java
+++ mavenproject1/src/main/java/com/mycompany/mavenproject1/automatic_detection_w_target_v2.java
@@ -1,3 +1,8 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package com.mycompany.mavenproject1;
 
 import java.awt.BorderLayout;
@@ -14,16 +19,28 @@ import org.opencv.core.Core;
 import org.opencv.core.CvType;
 import org.opencv.core.Mat;
 import org.opencv.core.MatOfPoint;
+import org.opencv.core.MatOfPoint2f;
 import org.opencv.core.Point;
+import org.opencv.core.Rect;
 import org.opencv.core.Scalar;
 import org.opencv.core.Size;
 import org.opencv.highgui.HighGui;
 import org.opencv.imgcodecs.Imgcodecs;
 import org.opencv.imgproc.Imgproc;
 
-class AutoHSVtarget {
+/**
+ *
+ * @author MSI
+ */
 
-   
+class automatic_detection {
+    
+    int target = 100;
+    int minDiff = target;
+    int optimalSaturation = 1;
+    boolean analysis_completed = false;
+    int s_val = 1;
+    
     private JFrame frame;
 
     private Mat src;
@@ -53,10 +70,9 @@ class AutoHSVtarget {
     private JSlider sliderLowV;
     private JSlider sliderHighV;
     
-
     private int i;
-
-    public AutoHSVtarget(String[] args) {
+    
+    public automatic_detection(String[] args) {
 
         src = Imgcodecs.imread("img\\carrote2.jpg");
         
@@ -76,8 +92,8 @@ class AutoHSVtarget {
         frame.setVisible(true);
         update();
     }
-
-     private void window_initialisation(Container pane) {
+    
+    private void window_initialisation(Container pane) {
        
         JPanel sliderPanel = new JPanel();
         
@@ -110,7 +126,7 @@ class AutoHSVtarget {
         pane.add(sliderPanel, BorderLayout.PAGE_START); 
 
     }
-
+    
     private void update() {
         
         while(true){
@@ -122,7 +138,7 @@ class AutoHSVtarget {
 
             Imgproc.cvtColor(resizeImg, srcHsv, Imgproc.COLOR_BGR2HSV,3);  
 
-            Core.inRange(srcHsv, new Scalar(sliderLowH.getValue(), sliderLowS.getValue(), sliderLowV.getValue()),
+            Core.inRange(srcHsv, new Scalar(sliderLowH.getValue(), s_val, sliderLowV.getValue()),
                         new Scalar(sliderHighH.getValue(), sliderHighS.getValue(), sliderHighV.getValue()), maskHsv);
 
             Imgproc.threshold(maskHsv,maskHsvInv,0, 255,Imgproc.THRESH_BINARY_INV );
@@ -136,33 +152,86 @@ class AutoHSVtarget {
             Mat hierarchy = new Mat();
 
             Imgproc.findContours(maskHsvInv, contours, hierarchy, Imgproc.RETR_EXTERNAL, Imgproc.CHAIN_APPROX_SIMPLE);
+            
+            List<ZoneCarotte> detected = new ArrayList<>();
+            //System.out.println("Detected " +detected.s);
+            
+            MatOfPoint2f[] contoursPoly  = new MatOfPoint2f[contours.size()];
+            Rect[] boundRect = new Rect[contours.size()];
+            
             Mat drawing = Mat.zeros(maskHsvInv.size(), CvType.CV_8UC3);
             for (int i = 0; i < contours.size(); i++) {
+                
+                contoursPoly[i] = new MatOfPoint2f(contours.get(i).toArray());
+                double peri = Imgproc.arcLength(contoursPoly[i], true);
+                Imgproc.approxPolyDP(contoursPoly[i], contoursPoly[i], 0.02 * peri, true);  
+                
+                boundRect[i] = Imgproc.boundingRect(new MatOfPoint(contoursPoly[i].toArray()));
+                ZoneCarotte newZone  = new ZoneCarotte(boundRect[i].x,boundRect[i].y,boundRect[i].x + boundRect[i].width,
+                                                        boundRect[i].y+boundRect[i].height,boundRect[i].width,boundRect[i].height);
+                
+                if ( newZone.getArea() > 1500 && !newZone.existsInArray(detected)){
+                    detected.add(newZone);
+                }
+                
                 Scalar color = new Scalar(rng.nextInt(256), rng.nextInt(256), rng.nextInt(256));
                 Imgproc.drawContours(drawing, contours, i, color, 1, Imgproc.LINE_8, hierarchy, 0, new Point());
             }
+            for (int k = 0; k < detected.size(); k++){
+                Point p1 = new Point(detected.get(k).upper_x, detected.get(k).upper_y);
+                Point p2 = new Point(detected.get(k).lower_x, detected.get(k).lower_y);
+                Scalar color = new Scalar(255, 0, 0);
+                 int thickness = 1;
+                Imgproc.rectangle(srcFinal, p1, p2, color, thickness);
+                
+            }
+            
+            if(!analysis_completed){
+                System.out.println(this.s_val);
+                if(this.s_val < 150){
+                    
+                    this.s_val += 2;
+                    
+                }else{
+                    
+                    analysis_completed =true;
+                    
+                }
+                
+                if(Math.abs(target - detected.size() + 1) < minDiff){
+                    
+                    minDiff = Math.abs(target - detected.size() + 1);
+                    optimalSaturation = this.s_val;
+                    
+                }
+                
+            }else{
+                    
+                    this.s_val = optimalSaturation;
+                    
+                }
 
             HighGui.imshow("original",resizeImg);
             HighGui.imshow("HSV",srcHsv);
             HighGui.imshow("mask", maskHsv);
             HighGui.imshow("maskInv", maskHsvInv);
-            HighGui.imshow("Final", srcFinal);
+            HighGui.imshow("RectFinal", srcFinal);
             HighGui.imshow("Final", drawing);
             HighGui.waitKey(1);
             
         }
        
     }
-
 }
 
-public class AutoHSVtargetDemo {
+
+public class automatic_detection_w_target_v2 {
 
     public static void main(String[] args) {
         // Load the native OpenCV library
         System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
 
-        new AutoHSVtarget(args);
+        new automatic_detection(args);
 
     }
 
